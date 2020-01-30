5738b2e0c2898fc5d4f05be2c774ee59a918bb57
youjin <yujin5836@gmail.com>
2020-01-31 01:20:03 +0900

    Fix : level3_box
    algorithm study

--- level3_box.c
+++ level3_box.c
@@ -1,7 +1,8 @@
+//clear 0130
 #include <stdio.h>
 int main(){
 
-    int i,j=0,k=0,q,w,l=0;
+    int i,j=0,k=0;
     int diff=1;
     int num=0;
     int color[1000]={0,};
@@ -26,9 +27,6 @@ int main(){
             }
         }
     }
-// color 배열 오름으로 정리
-
-
     for( i =0 ; i < num-1 ; i++){
 
         if(color[i] != color[i+1]){
@@ -47,43 +45,23 @@ int main(){
     }else if(diff>=6){
         printf("YES");
         return 0;         
-6
 
     }
 
 
-    printf("%d\n",diff);
-
-    for( i = 0 ; i < diff ; i++){
-        printf("%d ",cnt[i]);
-    }
-    printf("\n");
-
-    for( i = 0 ; i < num ; i++){
-        printf("%d ",color[i]);
-    }
-
-
-printf("\n");
-for(j=0 ; j <diff ;j++){
-    for(i=0;i<num;i++){
-        if(cnt[j] == color[i]){
+    for(j=0 ; j <diff ;j++){
+        for(i=0;i<num;i++){
+            if(cnt[j] == color[i]){
             bin[j]++;
+            }
         }
     }
-}
+
     for(i=0;i<diff;i++){
-    printf("%d ",bin[i]);
+        if(bin[i]==2){
+            fin++;
+        }
     }
-printf("\n");
-
-
-for(i=0;i<diff;i++){
-    if(bin[i]==2){
-        fin++;
-    }
-}
-printf("%d\n",fin);
 
     if(fin >= 3){
         printf("YES");
