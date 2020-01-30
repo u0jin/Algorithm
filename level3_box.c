1b908733c530cecc41d4c8887cd5faffefac3a55
youjin <yujin5836@gmail.com>
2020-01-31 01:08:33 +0900

    Update : level3_box
    test

--- level3_box.c
+++ level3_box.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 int main(){
 
-    int i,j=0;
+    int i,j=0,k=0,q,w,l=0;
     int diff=1;
     int num=0;
     int color[1000]={0,};
     int tmp;
+    int bin[5]={0,};
+    int fin=0;
     int cnt[1000]={0,}; 
 
     scanf("%d",&num);
@@ -26,26 +28,70 @@ int main(){
     }
 // color 배열 오름으로 정리
 
-    for(i=0;i<num;i++){
-        printf("%d %d\n",color[i],cnt[i]);
-    }
-//확인완료
 
     for( i =0 ; i < num-1 ; i++){
 
         if(color[i] != color[i+1]){
             diff++;
-            cnt[j] = color[i];
-            j++;
+            cnt[k] = color[i];
+            k++;
         }
     }
-// color 배열의 값이 다를경우 cnt 배열의 값으로 삽입
 
-    for(i=0;i<num;i++){   
-        printf("%d %d\n",color[i],cnt[i]);
+    cnt[k] = color[num-1];
+
+    if(diff < 3){
+        printf("NO");
+        return 0;
+        
+    }else if(diff>=6){
+        printf("YES");
+        return 0;         
+6
+
     }
 
-// 테스트
+
+    printf("%d\n",diff);
+
+    for( i = 0 ; i < diff ; i++){
+        printf("%d ",cnt[i]);
+    }
+    printf("\n");
+
+    for( i = 0 ; i < num ; i++){
+        printf("%d ",color[i]);
+    }
+
+
+printf("\n");
+for(j=0 ; j <diff ;j++){
+    for(i=0;i<num;i++){
+        if(cnt[j] == color[i]){
+            bin[j]++;
+        }
+    }
+}
+    for(i=0;i<diff;i++){
+    printf("%d ",bin[i]);
+    }
+printf("\n");
+
+
+for(i=0;i<diff;i++){
+    if(bin[i]==2){
+        fin++;
+    }
+}
+printf("%d\n",fin);
+
+    if(fin >= 3){
+        printf("YES");
+    }else{
+        printf("NO");
+    }
 
     return 0;
 }
+
+
