526e819de5dae56614d55a05bdcc54458e7b15ed
youjin <yujin5836@gmail.com>
2020-02-02 02:27:43 +0900

    Fix : level3_reverse1
    matrix reverse(0 -> 1)

---
+++ level3_reverse1.c
@@ -0,0 +1,41 @@
+//clear 0202
+#include <stdio.h>
+int main(){
+
+    int i,j;
+    int matrix[10][10]={0,};
+    int n;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++){
+        for(j=0;j<10;j++){
+            if(matrix[i][j] == 0){
+                matrix[i][j] = 1;
+            }else{
+                matrix[i][j] = 0;
+            }
+        }
+        for(j=0;j<10;j++){
+            if(matrix[j][i] == 0){
+                matrix[j][i] = 1;
+            }else{
+                matrix[j][i] = 0;
+            }
+        }
+        if(matrix[i][i] == 0){
+            matrix[i][i] = 1;
+        }
+        
+    }
+
+
+    for(i=0;i<10;i++){
+        for(j=0;j<10;j++){
+            printf("%d ",matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
